9cc9d681f501efd31df01cd74f80b18264856505
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-11-07 17:58:55 +0500

    task1

---
+++ Tyuiu.KovalenkoMA.Sprint4.Task1.V8.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KovalenkoMA.Sprint4.Task1.V8.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint4.Task1.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] a = { 8, 4, 7, 7, 3, 8, 8, 5, 8, 7, 2, 4 };
+            var res = ds.Calculate(a);
+            Assert.AreEqual(29, res);
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint4.Task1.V8/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KovalenkoMA.Sprint4.Task1.V8.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Коваленко М. А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #8                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ВВЕДИТЕ ИСХОДНЫЕ ДАННЫЕ:                                                *");
+        Console.WriteLine("***************************************************************************");
+
+        int len;
+        Console.WriteLine("Введите кол-во элементов массива: ");
+        len = Convert.ToInt32(Console.ReadLine());
+        int[] a = new int[len];
+        for (int i = 0; i <= len-1; i++)
+        {
+            Console.WriteLine("введите значение" + i + "элемента массива");
+            a[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(a));
+
+        Console.ReadKey();
+    }
+}
