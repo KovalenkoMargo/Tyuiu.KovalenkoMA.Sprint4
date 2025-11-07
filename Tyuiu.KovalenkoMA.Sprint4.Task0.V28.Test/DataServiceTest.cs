9a9a9865a11cbf531cb7ca26c81b6707ee3fdb10
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-11-07 16:16:05 +0500

    Add project files.

---
+++ Tyuiu.KovalenkoMA.Sprint4.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KovalenkoMA.Sprint4.Task0.V28.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint4.Task0.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] a = { 9, 8, 4, 6, 9, 4, 3, 6, 1, 2 };
+            var res = ds.GetMultEvenArrEl(a);
+            Assert.AreEqual(9216,res);
+
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint4.Task0.V28/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KovalenkoMA.Sprint4.Task0.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Коваленко М. А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #28                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int[] a = { 9, 8, 4, 6, 9, 4, 3, 6, 1, 2 };
+
+        Console.WriteLine("Дано: ");
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.WriteLine(a[i]);
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.GetMultEvenArrEl(a));
+
+        Console.ReadKey();
+
+    }
+}
