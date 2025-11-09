d0bfd9404c45143fd2f17c89450538cabc20f26a
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-11-09 19:34:52 +0500

    task7

---
+++ Tyuiu.KovalenkoMA.Sprint4.Task7.V15.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KovalenkoMA.Sprint4.Task7.V15.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint4.Task7.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int r = 4;
+            int c = 2;
+            int[,] mtx = new int[r, c];
+            string str = "10293847";
+            DataService ds = new DataService();
+            int res = ds.Calculate(r, c, str);
+            Assert.AreEqual(4, res);
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint4.Task7.V15/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KovalenkoMA.Sprint4.Task7.V15.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Коваленко М. А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #15                                                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int r = 4;
+        int c = 2;
+        int[,] mtx = new int[r, c];
+        string str = "10293847";
+        Console.WriteLine(str);
+        int index = 0;
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                Console.Write(str[index] + "\t");
+                index ++;
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(ds.Calculate(r, c, str));
+
+        Console.ReadKey();
+    }
+}
