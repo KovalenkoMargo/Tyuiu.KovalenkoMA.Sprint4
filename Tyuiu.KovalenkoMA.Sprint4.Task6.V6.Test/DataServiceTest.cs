4acb6e75bdb53602d0c9bb4ac2b99b34ac178bbe
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-11-09 18:52:49 +0500

    task6

---
+++ Tyuiu.KovalenkoMA.Sprint4.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KovalenkoMA.Sprint4.Task6.V6.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint4.Task6.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string[] mas = new string[] { "Борис", "Анна", "Михаил", "Ирина", "Сергей", "Татьяна", "Олег" };
+            var res = ds.Calculate(mas);
+            string[] mas1 = { "Борис", "Ирина"};
+            CollectionAssert.AreEqual(mas1, res);
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint4.Task6.V6/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.KovalenkoMA.Sprint4.Task6.V6.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Коваленко М. А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #6                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string[] mas = new string[] { "Борис", "Анна", "Михаил", "Ирина", "Сергей", "Татьяна", "Олег" };
+        for (int i = 0; i < mas.Length; i++)
+        {
+            Console.Write(mas[i] + "\t");
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        var mas2 = ds.Calculate(mas);
+ 
+        for (int i = 0; i < mas2.Length; i++)
+        {
+            Console.WriteLine(mas2[i]);
+        }
+
+        Console.ReadKey();
+    }
+}
